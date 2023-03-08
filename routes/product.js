99d8528b7b8b38635f97f26cfd7a5d273ec88b59
maria dakir <m.dakir@mundiapolis.ma>
2023-03-08 15:29:27 +0100

    add delete

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const productServices = require('../services/product');
 const router = express.Router();
 router.get('/', productServices.getAllProducts);
 router.get('/:productId', productServices.getProductById);
+router.delete('/:productId', productServices.deleteProduct);
 router.use('/add-product', productServices.postAddProduct);
 
 module.exports = router;
